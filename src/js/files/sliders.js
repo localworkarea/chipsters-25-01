9ab20d5885fb3e291c9750a5d9123cdabf06fb7a
localworkarea <localworkarea@gmail.com>
2025-01-24 13:03:09 +0200

    up

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -8,7 +8,7 @@
 // При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
 // Приклад: { Navigation, Autoplay }
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 /*
 Основні модулі слайдера:
 Navigation, Pagination, Autoplay, 
@@ -28,17 +28,27 @@ import "../../scss/base/swiper.scss";
 function initSliders() {
 	if (document.querySelector('.sec-01__slider')) { 
 		var swiper = new Swiper('.sec-01__slider', {
-				modules: [Navigation],
+				modules: [Navigation, Pagination],
 				observer: true,
 				observeParents: true,
 				slidesPerView: 1,
 				spaceBetween: 0,
 				speed: 800,
-				// effect: 'fade',
 				navigation: {
 						prevEl: '.sec-01__slider .swiper-button-prev',
 						nextEl: '.sec-01__slider .swiper-button-next',
 				},
+
+				// Пагинация
+			  pagination: {
+					el: '.swiper-pagination',
+					clickable: false,
+					// renderBullet: (index, className) => {
+					// 	return `<span class="${className}" data-index="${index}"></span>`;
+					// },
+				},
+				// --------
+
 				on: {
 						init: function () {
 								this.slides.forEach(function (slide, index) {
@@ -46,18 +56,74 @@ function initSliders() {
 										slide.setAttribute('id', slideId);
 										slide.classList.add('slide-' + (index + 1)); // Добавляем класс по номеру id
 								});
+
+								const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+			          const pagination = document.querySelector('.swiper-pagination');
+			          // Ширина контейнера пагинации
+			          const paginationWidth = 90; // px
+			          const bulletWidth = 16; // px
+			          const initialOffset = ((paginationWidth - bulletWidth) / 2) + 12;
+			          // Устанавливаем начальное смещение для всех буллетов
+			          bullets.forEach((bullet, index) => {
+			            // bullet.style.transform = `translateX(${initialOffset}px)`;
+			            bullet.style.left = `${initialOffset}px`;
+			          });
+			          // Устанавливаем начальные классы
+			          bullets[0].classList.add('swiper-pagination-bullet-active');
+			          bullets[1]?.classList.add('swiper-pagination-bullet-active-next');
+			          bullets[2]?.classList.add('swiper-pagination-bullet-active-next-next');
 						},
 						slideChange: function () {
-								var activeIndex = this.activeIndex;
-								var sectionBody = this.el.closest('.sec-01__body'); // Находим ближайший родительский элемент с классом section-body
-	
-								// Удаляем все классы слайдов из section-body
-								for (var i = 1; i <= swiper.slides.length; i++) {
-										sectionBody.classList.remove('slide-' + i);
-								}
-	
-								// Добавляем класс для текущего активного слайда
-								sectionBody.classList.add('slide-' + (activeIndex + 1)); 
+							var activeIndex = this.activeIndex;
+							var sectionBody = this.el.closest('.sec-01__body'); // Находим ближайший родительский элемент с классом section-body
+
+							// Удаляем все классы слайдов из section-body
+							for (var i = 1; i <= swiper.slides.length; i++) {
+									sectionBody.classList.remove('slide-' + i);
+							}
+							// Добавляем класс для текущего активного слайда
+							sectionBody.classList.add('slide-' + (activeIndex + 1)); 
+
+
+							// переключение и смещение буллетов =====================
+							const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+		          const activeIndex2 = swiper.activeIndex;
+
+		          // Ширина контейнера пагинации
+		          const paginationWidth = 90; // px
+		          const bulletWidth = 16; // px
+		          const initialOffset = ((paginationWidth - bulletWidth) / 2) + 12;
+
+		          // Смещение для всех буллетов
+		          const offset = initialOffset - activeIndex2 * bulletWidth;
+
+		          bullets.forEach((bullet, index) => {
+		            // Обновляем смещение
+		            // bullet.style.transform = `translateX(${offset}px)`;
+		            bullet.style.left = `${offset}px`;
+							
+		            // Удаляем старые классы
+		            bullet.classList.remove(
+		              'swiper-pagination-bullet-active-prev-prev',
+		              'swiper-pagination-bullet-active-prev',
+		              'swiper-pagination-bullet-active',
+		              'swiper-pagination-bullet-active-next',
+		              'swiper-pagination-bullet-active-next-next'
+		            );
+							
+		            // Устанавливаем новые классы
+		            if (index === activeIndex2) {
+		              bullet.classList.add('swiper-pagination-bullet-active');
+		            } else if (index === activeIndex2 - 1) {
+		              bullet.classList.add('swiper-pagination-bullet-active-prev');
+		            } else if (index === activeIndex2 - 2) {
+		              bullet.classList.add('swiper-pagination-bullet-active-prev-prev');
+		            } else if (index === activeIndex2 + 1) {
+		              bullet.classList.add('swiper-pagination-bullet-active-next');
+		            } else if (index === activeIndex2 + 2) {
+		              bullet.classList.add('swiper-pagination-bullet-active-next-next');
+		            }
+		          });
 						}
 				}
 		});
